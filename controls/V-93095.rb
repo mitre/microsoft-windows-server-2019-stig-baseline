8f7965200d7e923f69d309bdfd45300b126daf16
Jared Burns <burns.jared@gmail.com>
2020-05-13 09:47:49 -0400

    added Code to Controls
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-93095.rb
+++ controls/V-93095.rb
@@ -10,13 +10,9 @@ provide a trail of evidence in case the system or network is compromised.
 Collecting this data is essential for analyzing the security of information
 assets and detecting signs of suspicious and unexpected behavior.
 
-    Audit Policy Change records events related to changes in audit policy.
-
-
-  "
+    Audit Policy Change records events related to changes in audit policy."
   desc  "rationale", ""
-  desc  "check", "
-    Security Option \"Audit: Force audit policy subcategory settings (Windows
+  desc  'check', "Security Option \"Audit: Force audit policy subcategory settings (Windows
 Vista or later) to override audit policy category settings\" must be set to
 \"Enabled\" (WN19-SO-000050) for the detailed auditing subcategories to be
 effective.
@@ -32,22 +28,30 @@ as administrator\").
 
     If the system does not audit the following, this is a finding.
 
-    Policy Change >> Audit Policy Change - Failure
-  "
-  desc  "fix", "Configure the policy value for Computer Configuration >>
+    Policy Change >> Audit Policy Change - Failure"
+  desc  'fix', "Configure the policy value for Computer Configuration >>
 Windows Settings >> Security Settings >> Advanced Audit Policy Configuration >>
 System Audit Policies >> Policy Change >> \"Audit Audit Policy Change\" with
 \"Failure\" selected."
   impact 0.5
-  tag severity: nil
-  tag gtitle: "SRG-OS-000327-GPOS-00127"
-  tag satisfies: ["SRG-OS-000327-GPOS-00127", "SRG-OS-000458-GPOS-00203",
+  tag 'severity': nil
+  tag 'gtitle': 'SRG-OS-000327-GPOS-00127'
+  tag 'satisfies': ["SRG-OS-000327-GPOS-00127", "SRG-OS-000458-GPOS-00203",
 "SRG-OS-000463-GPOS-00207", "SRG-OS-000468-GPOS-00212"]
-  tag gid: "V-93095"
-  tag rid: "SV-103183r1_rule"
-  tag stig_id: "WN19-AU-000270"
-  tag fix_id: "F-99341r1_fix"
-  tag cci: ["CCI-000172", "CCI-002234"]
-  tag nist: ["AU-12 c", "AC-6 (9)", "Rev_4"]
+  tag 'gid': 'V-93095'
+  tag 'rid': 'SV-103183r1_rule'
+  tag 'stig_id': 'WN19-AU-000270'
+  tag 'fix_id': 'F-99341r1_fix'
+  tag 'cci': ["CCI-000172", "CCI-002234"]
+  tag 'nist': ["AU-12 c", "AC-6 (9)", "Rev_4"]
+
+  describe.one do
+    describe audit_policy do
+      its('Audit Policy Change') { should eq 'Failure' }
+    end
+    describe audit_policy do
+      its('Audit Policy Change') { should eq 'Success and Failure' }
+    end
+  end
 end
 
