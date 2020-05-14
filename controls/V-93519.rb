91c72b5e6bddb60ccc24108dede340900863740b
Jared Burns <burns.jared@gmail.com>
2020-05-14 09:49:02 -0400

    Updated the Comments
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-93519.rb
+++ controls/V-93519.rb
@@ -54,8 +54,8 @@ SecGuide.adml\" must be copied to the \\Windows\\PolicyDefinitions and
   tag cci: ["CCI-001084"]
   tag nist: ["SC-3", "Rev_4"]
 
-  domain_role = command('wmic computersystem get domainrole | Findstr /v DomainRole').stdout.strip
+  #domain_role = command('wmic computersystem get domainrole | Findstr /v DomainRole').stdout.strip
   
-    if domain_role == '4' || domain_role == '5'
+   # if domain_role == '4' || domain_role == '5'
 end
 
