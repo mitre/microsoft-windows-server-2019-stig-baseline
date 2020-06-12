f6c90812f3448fd6bf463f57ac44d478b9ed4c60
user <user@heimdal.fios-router.home>
2020-06-12 14:24:46 +0000

    syntax update

--- controls/V-93509.rb
+++ controls/V-93509.rb
@@ -1,10 +1,10 @@
 # encoding: UTF-8
 
-control "V-93509" do
-  title "Windows Server 2019 directory service must be configured to terminate LDAP-based network connections to the directory server after five minutes of inactivity."
-  desc  "The failure to terminate inactive network connections increases the risk of a successful attack on the directory server. The longer an established session is in progress, the more time an attacker has to hijack the session, implement a means to passively intercept data, or compromise any protections on client access. For example, if an attacker gains control of a client computer, an existing (already authenticated) session with the directory server could allow access to the directory. The lack of confidentiality protection in LDAP-based sessions increases exposure to this vulnerability."
-  desc  "rationale", ""
-  desc  "check", "This applies to domain controllers. It is NA for other systems.
+control 'V-93509' do
+  title 'Windows Server 2019 directory service must be configured to terminate LDAP-based network connections to the directory server after five minutes of inactivity.'
+  desc  'The failure to terminate inactive network connections increases the risk of a successful attack on the directory server. The longer an established session is in progress, the more time an attacker has to hijack the session, implement a means to passively intercept data, or compromise any protections on client access. For example, if an attacker gains control of a client computer, an existing (already authenticated) session with the directory server could allow access to the directory. The lack of confidentiality protection in LDAP-based sessions increases exposure to this vulnerability.'
+  desc  'rationale', ''
+  desc  'check', 'This applies to domain controllers. It is NA for other systems.
     Open an elevated \"Command Prompt\" (run as administrator).
     Enter \"ntdsutil\".
     At the \"ntdsutil:\" prompt, enter \"LDAP policies\".
@@ -22,8 +22,8 @@ control "V-93509" do
     dsquery * \"cn=Default Query Policy,cn=Query-Policies,cn=Directory Service, cn=Windows NT,cn=Services,cn=Configuration,dc=[forest-name]\" -attr LDAPAdminLimits
 
     The quotes are required and dc=[forest-name] is the fully qualified LDAP name of the domain being reviewed (e.g., dc=disaost,dc=mil).
-    If the results do not specify a \"MaxConnIdleTime\" or it has a value greater than \"300\" (5 minutes), this is a finding."
-  desc  "fix", "Configure the directory service to terminate LDAP-based network connections to the directory server after 5 minutes of inactivity.
+    If the results do not specify a \"MaxConnIdleTime\" or it has a value greater than \"300\" (5 minutes), this is a finding.'
+  desc  'fix', 'Configure the directory service to terminate LDAP-based network connections to the directory server after 5 minutes of inactivity.
     Open an elevated \"Command prompt\" (run as administrator).
     Enter \"ntdsutil\".
     At the \"ntdsutil:\" prompt, enter \"LDAP policies\".
@@ -33,16 +33,16 @@ control "V-93509" do
     At the \"ldap policy:\" prompt, enter \"Set MaxConnIdleTime to 300\".
     Enter \"Commit Changes\" to save.
     Enter \"Show values\" to verify changes.
-    Enter \"q\" at the \"ldap policy:\" and \"ntdsutil:\" prompts to exit."
+    Enter \"q\" at the \"ldap policy:\" and \"ntdsutil:\" prompts to exit.'
   impact 0.3
-  tag severity: nil
-  tag gtitle: "SRG-OS-000163-GPOS-00072"
-  tag gid: "V-93509"
-  tag rid: "SV-103595r1_rule"
-  tag stig_id: "WN19-DC-000160"
-  tag fix_id: "F-99753r1_fix"
-  tag cci: ["CCI-001133"]
-  tag nist: ["SC-10", "Rev_4"]
+  tag 'severity': ''
+  tag 'gtitle': "SRG-OS-000163-GPOS-00072"
+  tag 'gid': "V-93509"
+  tag 'rid': "SV-103595r1_rule"
+  tag 'stig_id': "WN19-DC-000160"
+  tag 'fix_id': "F-99753r1_fix"
+  tag 'cci': ["CCI-001133"]
+  tag 'nist': ["SC-10", "Rev_4"]
 
   #control "V-14831" Windows 2012 Profile
 
@@ -54,8 +54,8 @@ control "V-93509" do
   #forrest = attribute('forrest')
 
   #Temporarily hardcoded value for testing | Need to set it to input based upon approval
-  forest_name = 'corp.contoso.com'.split('.')join(",dc=")
-  print(forest_name)
+  forest_name = 'corp.contoso.com'.split('.').join(",dc=")
+  #print(forest_name)
 
   domain_role = command('wmic computersystem get domainrole | Findstr /v DomainRole').stdout.strip
   if domain_role == '4' || domain_role == '5'
