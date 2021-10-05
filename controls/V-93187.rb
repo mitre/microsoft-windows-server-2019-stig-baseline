a50dfb7c4ab8d3aec92f176b9623ac7a45c1b72f
Aaron Lippold <lippold@gmail.com>
2021-10-05 20:35:18 +0000

    moved the custom resource into the libraries dir added a .gitignore file
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/V-93187.rb
+++ controls/V-93187.rb
@@ -57,39 +57,6 @@ control 'V-93187' do
   tag 'cci': ['CCI-001891']
   tag 'nist': ['AU-8 (1) (a)', 'Rev_4']
 
-  begin
-        require 'inspec/resource'
-        require 'inspec/resource/registry_key'
-  rescue LoadError
-      end
-
-  class W32timeConfig < Inspec.resource(1)
-    name 'w32time_config'
-    supports platform: 'windows'
-    desc 'Tests Win32Time configuration on Windows'
-    example <<~EXAMPLE
-        describe win32time_config do
-          its("type") { should cmp "NT5DS" }
-        end
-      EXAMPLE
-
-    def initialize
-      @path = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters'
-    end
-
-    def ntpserver
-      inspec.registry_key(@path).NtpServer.split
-    end
-
-    def type
-      inspec.registry_key(@path).type
-    end
-
-    def to_s
-      'w32time_config'
-    end
-  end
-
   domain_role = command('wmic computersystem get domainrole | Findstr /v DomainRole').stdout.strip
 
   if domain_role == '4' || domain_role == '5'
