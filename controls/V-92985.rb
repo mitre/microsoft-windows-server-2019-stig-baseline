8bf5994842b7f24fa3e705c4696ecb0b0f239774
Adam Giard <agiard@mitre.org>
2021-10-19 13:49:12 -0700

    Add conditional per domain role

--- controls/V-92985.rb
+++ controls/V-92985.rb
@@ -1,6 +1,4 @@
-# encoding: UTF-8
-
-control "V-92985" do
+control 'V-92985' do
   title "Windows Server 2019 must be configured to audit Account Management -
 Computer Account Management successes."
   desc  "Maintaining an audit trail of system activity logs can help identify
@@ -12,7 +10,7 @@ assets and detecting signs of suspicious and unexpected behavior.
 
     Computer Account Management records events such as creating, changing,
 deleting, renaming, disabling, or enabling computer accounts."
-  desc  "rationale", ""
+  desc  'rationale', ''
   desc  'check', "This applies to domain controllers. It is NA for other systems.
 
     Security Option \"Audit: Force audit policy subcategory settings (Windows
@@ -39,25 +37,35 @@ Management\" with \"Success\" selected."
   impact 0.5
   tag 'severity': nil
   tag 'gtitle': 'SRG-OS-000004-GPOS-00004'
-  tag 'satisfies': ["SRG-OS-000004-GPOS-00004", "SRG-OS-000239-GPOS-00089",
-"SRG-OS-000240-GPOS-00090", "SRG-OS-000241-GPOS-00091",
-"SRG-OS-000303-GPOS-00120", "SRG-OS-000476-GPOS-00221"]
+  tag 'satisfies': %w(SRG-OS-000004-GPOS-00004 SRG-OS-000239-GPOS-00089
+SRG-OS-000240-GPOS-00090 SRG-OS-000241-GPOS-00091
+SRG-OS-000303-GPOS-00120 SRG-OS-000476-GPOS-00221)
   tag 'gid': 'V-92985'
   tag 'rid': 'SV-103073r1_rule'
   tag 'stig_id': 'WN19-DC-000230'
   tag 'fix_id': 'F-99231r1_fix'
-  tag 'cci': ["CCI-000018", "CCI-000172", "CCI-001403", "CCI-001404",
-"CCI-001405", "CCI-002130"]
-  tag 'nist': ["AC-2 (4)", "AU-12 c", "AC-2 (4)", "AC-2 (4)", "AC-2 (4)", "AC-2
-(4)", "Rev_4"]
+  tag 'cci': %w(CCI-000018 CCI-000172 CCI-001403 CCI-001404
+CCI-001405 CCI-002130)
+  tag 'nist': ['AC-2 (4)', 'AU-12 c', 'AC-2 (4)', 'AC-2 (4)', 'AC-2 (4)', "AC-2
+(4)", 'Rev_4']
 
-  describe.one do
-    describe audit_policy do
-      its('Computer Account Management') { should eq 'Success' }
+  domain_role = command('wmic computersystem get domainrole | Findstr /v DomainRole').stdout.strip
+  case domain_role
+  when '4', '5'
+    impact 0.5
+    describe.one do
+      describe audit_policy do
+        its('Computer Account Management') { should eq 'Success' }
+      end
+      describe audit_policy do
+        its('Computer Account Management') { should eq 'Success and Failure' }
+      end
     end
-    describe audit_policy do
-      its('Computer Account Management') { should eq 'Success and Failure' }
+  when '2', '3'
+    impact 0.0
+    desc 'This system is exempt from this control'
+    describe 'This system is exempt from this control' do
+      skip 'This system is exempt from this control'
     end
   end
 end
-
