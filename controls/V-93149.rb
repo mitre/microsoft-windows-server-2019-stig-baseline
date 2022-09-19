25dec71bebb4090d74f6255f861f8641ea8d7d3e
karikarshivani <karikarshivani@gmail.com>
2022-09-19 11:01:49 -0400

    Modified input reference to display all elements in the array
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/V-93149.rb
+++ controls/V-93149.rb
@@ -14,12 +14,12 @@ control "V-93149" do
     Value Type: REG_SZ
     Value: See message title options below
 
-    \"#{input('LegalNoticeCaption')[0]}\", \"#{input('LegalNoticeCaption')[1]}\", or an organization-defined equivalent.
+    \"#{input('LegalNoticeCaption').join("\", \"")}\", or an organization-defined equivalent.
 
     If an organization-defined title is used, it can in no case contravene or modify the language of the banner text required in WN19-SO-000150.
 
     Automated tools may only search for the titles defined above. If an organization-defined title is used, a manual review will be required."
-  desc  'fix', "Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> \"Interactive Logon: Message title for users attempting to log on\" to \"#{input('LegalNoticeCaption')[0]}\", \"#{input('LegalNoticeCaption')[1]}\", or an organization-defined equivalent.
+  desc  'fix', "Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> \"Interactive Logon: Message title for users attempting to log on\" to \"#{input('LegalNoticeCaption').join("\", \"")}\", or an organization-defined equivalent.
 
     If an organization-defined title is used, it can in no case contravene or modify the language of the message text required in WN19-SO-000150."
   impact 0.3
