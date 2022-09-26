1c04a91a48b242c8cc9b65127244885c9d1e46b6
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-09-26 19:47:47 -0400

    Parameterized V-93285 based on #{input('maximum_password_age_machine')}
    Except for author's "this is the default configuration" and the examples with hexidecimal values for default of 30. Left this as-is.

--- controls/V-93285.rb
+++ controls/V-93285.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 control "V-93285" do
-  title "Windows Server 2019 maximum age for machine account passwords must be configured to 30 days or less."
+  title "Windows Server 2019 maximum age for machine account passwords must be configured to #{input('maximum_password_age_machine')} days or less."
   desc  "Computer account passwords are changed automatically on a regular basis. This setting controls the maximum password age that a machine account may have. This must be set to no more than 30 days, ensuring the machine changes its password monthly."
   desc  "rationale", ""
   desc  "check", "This is the default configuration for this setting (30 days).
@@ -16,7 +16,7 @@ control "V-93285" do
     Value Type: REG_DWORD
     Value: 0x0000001e (30) (or less, but not 0)"
   desc  "fix", "This is the default configuration for this setting (30 days).
-    Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> \"Domain member: Maximum machine account password age\" to \"30\" or less (excluding \"0\", which is unacceptable)."
+    Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> \"Domain member: Maximum machine account password age\" to \"#{input('maximum_password_age_machine')}\" or less (excluding \"0\", which is unacceptable)."
   impact 0.5
   tag severity: nil
   tag gtitle: "SRG-OS-000480-GPOS-00227"
