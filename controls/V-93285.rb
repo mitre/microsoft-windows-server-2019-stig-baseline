86437f3f6d97a370d96ce8cfc573eeecb07b3f1f
karikarshivani <karikarshivani@gmail.com>
2022-09-27 20:15:25 -0400

    Parameterized 30 (except hex for DWORD)
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/V-93285.rb
+++ controls/V-93285.rb
@@ -2,9 +2,9 @@
 
 control "V-93285" do
   title "Windows Server 2019 maximum age for machine account passwords must be configured to #{input('maximum_password_age_machine')} days or less."
-  desc  "Computer account passwords are changed automatically on a regular basis. This setting controls the maximum password age that a machine account may have. This must be set to no more than 30 days, ensuring the machine changes its password monthly."
+  desc  "Computer account passwords are changed automatically on a regular basis. This setting controls the maximum password age that a machine account may have. This must be set to no more than #{input('maximum_password_age_machine')} days, ensuring the machine changes its password monthly."
   desc  "rationale", ""
-  desc  "check", "This is the default configuration for this setting (30 days).
+  desc  "check", "This is the default configuration for this setting (#{input('maximum_password_age_machine')} days).
 
     If the following registry value does not exist or is not configured as specified, this is a finding:
 
@@ -14,8 +14,8 @@ control "V-93285" do
     Value Name: MaximumPasswordAge
 
     Value Type: REG_DWORD
-    Value: 0x0000001e (30) (or less, but not 0)"
-  desc  "fix", "This is the default configuration for this setting (30 days).
+    Value: 0x0000001e (#{input('maximum_password_age_machine')}) (or less, but not 0)"
+  desc  "fix", "This is the default configuration for this setting (#{input('maximum_password_age_machine')} days).
     Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> \"Domain member: Maximum machine account password age\" to \"#{input('maximum_password_age_machine')}\" or less (excluding \"0\", which is unacceptable)."
   impact 0.5
   tag severity: nil
