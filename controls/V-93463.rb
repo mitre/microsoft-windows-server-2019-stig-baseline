c24567cb3da1f029b159e3621e6d830ee87b673b
karikarshivani <karikarshivani@gmail.com>
2022-09-27 20:01:13 -0400

    Parameterized minimum_password_length
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/V-93463.rb
+++ controls/V-93463.rb
@@ -1,19 +1,19 @@
 # encoding: UTF-8
 
 control "V-93463" do
-  title "Windows Server 2019 minimum password length must be configured to 14 characters."
+  title "Windows Server 2019 minimum password length must be configured to #{input('minimum_password_length')} characters."
   desc  "Information systems not protected with strong password schemes (including passwords of minimum length) provide the opportunity for anyone to crack the password, thus gaining access to the system and compromising the device, information, or the local network."
   desc  "rationale", ""
   desc  "check", "Verify the effective setting in Local Group Policy Editor.
 
     Run \"gpedit.msc\".
     Navigate to Local Computer Policy >> Computer Configuration >> Windows Settings >> Security Settings >> Account Policies >> Password Policy.
-    If the value for the \"Minimum password length,\" is less than \"14\" characters, this is a finding.
+    If the value for the \"Minimum password length,\" is less than \"#{input('minimum_password_length')}\" characters, this is a finding.
 
     For server core installations, run the following command:
     Secedit /Export /Areas SecurityPolicy /CFG C:\\Path\\FileName.Txt
-    If \"MinimumPasswordLength\" is less than \"14\" in the file, this is a finding."
-  desc  "fix", "Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Account Policies >> Password Policy >> \"Minimum password length\" to \"14\" characters."
+    If \"MinimumPasswordLength\" is less than \"#{input('minimum_password_length')}\" in the file, this is a finding."
+  desc  "fix", "Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Account Policies >> Password Policy >> \"Minimum password length\" to \"#{input('minimum_password_length')}\" characters."
   impact 0.5
   tag severity: nil
   tag gtitle: "SRG-OS-000078-GPOS-00046"
