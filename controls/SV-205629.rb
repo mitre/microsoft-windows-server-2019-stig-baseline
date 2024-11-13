242fcf76a59ecbf320b5385eb962ccde683f3ded
Daniel Medina <dmedina@mitre.org>
2024-11-13 12:41:32 -0500

    adjust text

--- controls/SV-205629.rb
+++ controls/SV-205629.rb
@@ -1,20 +1,20 @@
 control 'SV-205629' do
-  title 'Windows Server 2019 must have the number of allowed bad logon attempts configured to three or less.'
-  desc 'The account lockout feature, when enabled, prevents brute-force password attacks on the system. The higher this value is, the less effective the account lockout feature will be in protecting the local system. The number of bad logon attempts must be reasonably small to minimize the possibility of a successful password attack while allowing for honest errors made during normal user logon.'
-  desc 'check', 'Verify the effective setting in Local Group Policy Editor.
+  title "Windows Server 2019 must have the number of allowed bad logon attempts configured to #{input('max_pass_lockout')} or less."
+  desc "The account lockout feature, when enabled, prevents brute-force password attacks on the system. The higher this value is, the less effective the account lockout feature will be in protecting the local system. The number of bad logon attempts must be reasonably small to minimize the possibility of a successful password attack while allowing for honest errors made during normal user logon."
+  desc 'check', "Verify the effective setting in Local Group Policy Editor.
 
-Run "gpedit.msc".
+Run \"gpedit.msc\".
 
 Navigate to Local Computer Policy >> Computer Configuration >> Windows Settings >> Security Settings >> Account Policies >> Account Lockout Policy.
 
-If the "Account lockout threshold" is "0" or more than "3" attempts, this is a finding.
+If the \"Account lockout threshold\" is \"0\" or more than \"#{input('max_pass_lockout')}\" attempts, this is a finding.
 
 For server core installations, run the following command:
 
 Secedit /Export /Areas SecurityPolicy /CFG C:\\Path\\FileName.Txt
 
-If "LockoutBadCount" equals "0" or is greater than "3" in the file, this is a finding.'
-  desc 'fix', 'Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Account Policies >> Account Lockout Policy >> "Account lockout threshold" to "3" or fewer invalid logon attempts (excluding "0", which is unacceptable).'
+If \"LockoutBadCount\" equals \"0\" or is greater than \"#{input('max_pass_lockout')}\" in the file, this is a finding."
+  desc 'fix', "Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Account Policies >> Account Lockout Policy >> \"Account lockout threshold\" to \"#{input('max_pass_lockout')}\" or fewer invalid logon attempts (excluding \"0\", which is unacceptable)."
   impact 0.5
   ref 'DPMS Target Microsoft Windows Server 2019'
   tag severity: 'medium'
--- controls/SV-205710.rb
+++ controls/SV-205710.rb
@@ -1,6 +1,6 @@
 control 'SV-205710' do
-  title 'Windows Server 2019 must automatically remove or disable emergency accounts after the crisis is resolved or within 72 hours.'
-  desc 'Emergency administrator accounts are privileged accounts established in response to crisis situations where the need for rapid account activation is required. Therefore, emergency account activation may bypass normal account authorization processes. If these accounts are automatically disabled, system maintenance during emergencies may not be possible, thus adversely affecting system availability.
+  title "Windows Server 2019 must automatically remove or disable emergency accounts after the crisis is resolved or within #{input('emergency_account_period')*24} hours."
+  desc "Emergency administrator accounts are privileged accounts established in response to crisis situations where the need for rapid account activation is required. Therefore, emergency account activation may bypass normal account authorization processes. If these accounts are automatically disabled, system maintenance during emergencies may not be possible, thus adversely affecting system availability.
 
 Emergency administrator accounts are different from infrequently used accounts (i.e., local logon accounts used by system administrators when network or normal logon/access is not available). Infrequently used accounts are not subject to automatic termination dates. Emergency accounts are accounts created in response to crisis situations, usually for use by maintenance personnel. The automatic expiration or disabling time period may be extended as needed until the crisis is resolved; however, it must not be extended indefinitely. A permanent account should be established for privileged users who need long-term maintenance accounts.
 
@@ -13,24 +13,24 @@ If emergency administrator accounts have not been configured with an expiration 
 
 Domain Controllers:
 
-Open "PowerShell".
+Open \"PowerShell\".
 
-Enter "Search-ADAccount -AccountExpiring | FT Name, AccountExpirationDate".
+Enter \"Search-ADAccount -AccountExpiring | FT Name, AccountExpirationDate\".
 
-If "AccountExpirationDate" has been defined and is not within 72 hours for an emergency administrator account, this is a finding.
+If \"AccountExpirationDate\" has been defined and is not within #{input('emergency_account_period')*24} hours for an emergency administrator account, this is a finding.
 
 Member servers and standalone or nondomain-joined systems:
 
-Open "Command Prompt".
+Open \"Command Prompt\".
 
-Run "Net user [username]", where [username] is the name of the emergency account.
+Run \"Net user [username]\", where [username] is the name of the emergency account.
 
-If "Account expires" has been defined and is not within 72 hours for an emergency administrator account, this is a finding.'
-  desc 'fix', 'Remove emergency administrator accounts after a crisis has been resolved or configure the accounts to automatically expire within 72 hours.
+If \"Account expires\" has been defined and is not within #{input('emergency_account_period')*24} hours for an emergency administrator account, this is a finding."
+  desc 'fix', "Remove emergency administrator accounts after a crisis has been resolved or configure the accounts to automatically expire within #{input('emergency_account_period')*24} hours.
 
-Domain accounts can be configured with an account expiration date, under "Account" properties.
+Domain accounts can be configured with an account expiration date, under \"Account\" properties.
 
-Local accounts can be configured to expire with the command "Net user [username] /expires:[mm/dd/yyyy]", where username is the name of the temporary user account.'
+Local accounts can be configured to expire with the command \"Net user [username] /expires:[mm/dd/yyyy]\", where username is the name of the temporary user account."
   impact 0.5
   ref 'DPMS Target Microsoft Windows Server 2019'
   tag severity: 'medium'
